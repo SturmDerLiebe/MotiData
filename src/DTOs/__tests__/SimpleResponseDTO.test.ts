746e477021a176aa45ee465da63dc776eeb85113
Christian Sturm <c.sturm@avm.de>
2025-01-17 00:05:48 +0100

    test: add test for SimpleResponseDTO helper

---
+++ src/DTOs/__tests__/SimpleResponseDTO.test.ts
@@ -0,0 +1,39 @@
+import { expect, test } from "vitest";
+import {
+    extractDataBasedOnContentType,
+    transformToSimpleResponse,
+    type SimpleResponse,
+} from "../SimpleResponseDTO.js";
+
+test("should transform to SimpleResponseDTO", async () => {
+    //GIVEN
+    const EXPECTED_STATUS = 200;
+    const EXPECTED_DATA = { aKey: "aValue" };
+    const RESPONSE = new Response(JSON.stringify(EXPECTED_DATA), {
+        status: EXPECTED_STATUS,
+        headers: new Headers({ "Content-Type": "application/json" }),
+    });
+
+    //WHEN
+    const ACTUAL =
+        await transformToSimpleResponse<typeof EXPECTED_DATA>(RESPONSE);
+
+    //THEN
+    expect(ACTUAL).toEqual<SimpleResponse<typeof EXPECTED_DATA>>({
+        ok: RESPONSE.ok,
+        statusCode: RESPONSE.status,
+        data: EXPECTED_DATA,
+    });
+});
+
+test("should handle default, non json response", async () => {
+    //GIVEN
+    const EXPECTED_DATA = "12345";
+    const RESPONSE = new Response(EXPECTED_DATA);
+
+    //WHEN
+    const ACTUAL = await extractDataBasedOnContentType(RESPONSE);
+
+    //THEN
+    expect(ACTUAL).toEqual(EXPECTED_DATA);
+});
