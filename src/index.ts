f4139cacee1bffea1940a701285cfde0ed55e773
Christian Sturm <c.sturm@avm.de>
2025-01-18 12:07:29 +0100

    feat: export missing files from index.js

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+// Repositories:
 export type { SessionRepositoryInterface } from "./repositories/SessionRepositoryInterface.ts";
 export { UserRepository } from "./repositories/UserRepository.js";
+//DTOs:
+export type { SimpleResponse } from "./DTOs/SimpleResponseDTO.ts";
+export type { RegistrationDTO } from "./DTOs/RegistrationDTO.ts";
+export type { UserDetailsDTO } from "./DTOs/UserDetailsDTO.ts";
+//DTO-Helpers:
+export { SimpleResponseHelpers } from "./DTOs/SimpleResponseDTO.js";
+export { RegistrationHelper } from "./DTOs/RegistrationDTO.js";
+// Miscellanieous Types:
+export type { BaseRepositoryConstructorParam } from "./repositories/BaseRepository.ts";
